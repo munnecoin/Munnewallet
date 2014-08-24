5908bd15dd4f98cc520827813911968561cfd17b
Pieter Wuille <pieter.wuille@gmail.com>
2014-08-24 02:08:25 +0200

    Make CScript::clear() release its memory

--- src/script.h
+++ src/script.h
@@ -577,6 +577,12 @@ public:
     {
         return CScriptID(Hash160(*this));
     }
+
+    void clear()
+    {
+        // The default std::vector::clear() does not release memory.
+        std::vector<unsigned char>().swap(*this);
+    }
 };
 
 
